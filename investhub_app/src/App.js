f3b6302c79a979c859b35b4954968703a749a4ab
Dean Schwartz <deanschwartz2@gmail.com>
2022-02-23 21:19:50 -0800

    Small app.js text change

--- investhub_app/src/App.js
+++ investhub_app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Invest Hub Site
+          InvestHub Site
         </a>
       </header>
     </div>
